4e922f7aca8f76a1699d62f4ede7510b64c4081c
johny <johny@DESKTOP-ALUGM9L>
2021-05-28 17:05:24 -0300

    Adicionar arquivos de projeto.

---
+++ Remote MySQL/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Remote_MySQL
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string connStr = "server=localhost;user=dev;database=dev;port=3306;password=123456";
+            MySqlConnection conn = new MySqlConnection( connStr );
+
+            try
+            {
+                Console.WriteLine( "Conectando..." );
+                conn.Open();
+
+                string sql = "SELECT id, nome, idade FROM pessoa;";
+                MySqlCommand cmd = new MySqlCommand( sql, conn );
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine( $"{rdr[ 0 ]} --- {rdr[ 1 ]} --- {rdr[ 2 ]}" );
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine( ex.ToString() );
+            }
+
+            conn.Close();
+        }
+    }
+}
